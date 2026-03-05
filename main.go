fb76eb06bcc62b8b434acd02d9513e120c6dab57
unknown <kholiqfadhilah2208@gmail.com>
2026-03-05 13:49:42 +0700

    Update sumber lagi.

--- main.go
+++ main.go
@@ -224,7 +224,7 @@ func getHTML(targetUrl string) (*goquery.Document, error) {
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8")
 	req.Header.Set("Accept-Language", "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7")
-	req.Header.Set("Referer", "https://otakudesu.blog/") // Pura-pura datang dari halaman home
+	req.Header.Set("Referer", "https://otakudesu.blog/")
 	req.Header.Set("Sec-Ch-Ua", "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"")
 	req.Header.Set("Sec-Ch-Ua-Mobile", "?0")
 	req.Header.Set("Sec-Ch-Ua-Platform", "\"Windows\"")
